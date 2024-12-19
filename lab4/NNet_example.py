3d3218b21b11fa88c8d121549aa0eb197cbaf3e3
Matsvei Alkhimovich <matalhim@gmail.com>
2024-12-19 12:13:33 +0300

    modified:   lab4/NNet_example.py 	modified:   lab4/main.ipynb 	new file:   lab4/task.ipynb

--- lab4/NNet_example.py
+++ lab4/NNet_example.py
@@ -1,8 +1,10 @@
 
 import random
 import time
+
 import numpy as np
 from sklearn.metrics import r2_score
+
 
 class Network(object):
 
@@ -20,12 +22,13 @@ class Network(object):
         n = len(training_data)
         for j in range(epochs):
             random.shuffle(training_data)
-            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            mini_batches = [training_data[k:k+mini_batch_size]
+                            for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
                 pass
-                #self.evaluate((test_data), n_test)
+                # self.evaluate((test_data), n_test)
             else:
                 print("Epoch {0} complete".format(j))
 
@@ -46,15 +49,16 @@ class Network(object):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
         activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        activations = [x]  # list to store all the activations, layer by layer
+        zs = []  # list to store all the z vectors, layer by layer
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
             activation = self.sigmoid(z)
             activations.append(activation)
         # backward pass
-        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
+        delta = self.cost_derivative(
+            activations[-1], y) * self.sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         for l in range(2, self.num_layers):
@@ -77,8 +81,9 @@ class Network(object):
     def cost_derivative(self, output_activations, y):
         return (output_activations-y)
 
-    def sigmoid(self,z):
+    def sigmoid(self, z):
         return 1.0/(1.0+np.exp(-z))
+
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
@@ -86,31 +91,28 @@ class Network(object):
             a = self.sigmoid(np.dot(w, a)+b)
         return a
 
-    def sigmoid_prime(self,z):
+    def sigmoid_prime(self, z):
         """Derivative of the sigmoid function."""
         return self.sigmoid(z)*(1-self.sigmoid(z))
 
 
 if __name__ == "__main__":
-    net = Network([3,6,1])
+    net = Network([3, 6, 1])
     data = np.array([])
 
-    data = np.loadtxt("data.txt", dtype=float)
+    data = np.loadtxt("lab4/data.txt", dtype=float)
     training_data = []
     for value in data:
-        inputs = value[0:-1].reshape(3,1)
-        outputs = value[-1].reshape(1,1)
+        inputs = value[0:-1].reshape(3, 1)
+        outputs = value[-1].reshape(1, 1)
         xj = (inputs, outputs)
         training_data.append(xj)
 
-
     net.SGD(training_data, 500, len(training_data),  2)
-
-
 
     for entry in training_data:
         received_value = net.feedforward(entry[0])
         print(" Expected: ", entry[1], " Received : ", received_value)
 
-    ans = net.evaluate(training_data) # r2 score calculation
+    ans = net.evaluate(training_data)
     print(ans)
